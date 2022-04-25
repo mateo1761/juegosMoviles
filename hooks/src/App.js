df311286380218de133adfc707dca759b5990152
mateo1761 <mateo.quintero@manpowergroup.com.co>
2022-04-25 17:10:40 -0500

    Se finaliza ejemplo de hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contador from './componentes/Contador';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo en react</h1>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
