a8337c644a47c6e034cfb4d3b88a753fc7b23a42
mateo1761 <mateo.quintero@manpowergroup.com.co>
2022-04-26 08:42:47 -0500

    Se finaliza 2 ejercicio de useEffect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Contador from './componentes/Contador';
 import Control from './componentes/Control'
 import Efecto from './componentes/ContaEffect'
+import Nosotros from './componentes/Nosotros';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Contador/>
       <Control/>
       <Efecto/>
+      <Nosotros/>
     </div>
   );
 }
